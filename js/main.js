66b00f17f5fa92f74ada23158c6e9eb8c44755a4
Nikita <nikita.krymov@bk.ru>
2021-10-03 12:49:06 +0300

    Поправил значение линтера

--- js/main.js
+++ js/main.js
@@ -1,61 +1,60 @@
 const getRandNumber = function (min,max,floatPoint) { // число с плавающей точкой из диапазона "от...до" с указанным "количеством знаков после запятой"
-  rand_num = min + Math.random() * (max + 1 - min);
+  let rand_num = min + Math.random() * (max + 1 - min);
   if (min >= max) {
     return "Заданы некорректные параметры!"
   }
   return rand_num.toFixed(floatPoint) // округление до n-ых знаков после запятой
-}
-
+};
 const avatar = [
-  img/avatars/user01.png,
-  img/avatars/user02.png,
-  img/avatars/user03.png,
-  img/avatars/user04.png,
-  img/avatars/user05.png,
-  img/avatars/user06.png,
-  img/avatars/user07.png,
-  img/avatars/user08.png,
-  img/avatars/user09.png,
-  img/avatars/user10.png
-]
+  'img/avatars/user01.png',
+  'img/avatars/user02.png',
+  'img/avatars/user03.png',
+  'img/avatars/user04.png',
+  'img/avatars/user05.png',
+  'img/avatars/user06.png',
+  'img/avatars/user07.png',
+  'img/avatars/user08.png',
+  'img/avatars/user09.png',
+  'img/avatars/user10.png',
+];
 const Type = [
   palace,
   flat,
   house,
   bungalow,
-  hotel
-]
+  hotel,
+];
 const CheckIn = [
-  "12:00",
-  "13:00",
-  "14:00"
-]
+  '12:00',
+  '13:00',
+  '14:00',
+];
 const CheckOut = [
-  "12:00",
-  "13:00",
-  "14:00"
-]
+  '12:00',
+  '13:00',
+  '14:00',
+];
 const Feat = [
-  "wifi",
-  "dishwasher",
-  "parking",
-  "washer",
-  "elevator",
-  "conditioner"
-]
+  'wifi',
+  'dishwasher',
+  'parking',
+  'washer',
+  'elevator',
+  'conditioner',
+];
 const PhoTos = [
-  "https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg",
-  "https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg",
-  "https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg"
-]
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg',
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg',
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg',
+];
 const getRandom = (element) => {
   return element[_.random(0,element.length - 1)];
-}
+};
 const autor = () => {
   return {
     avatars: getRandom(avatar)
   }
-}
+};
 const offer = {
   title: Keksobook,
   address: (35.67983, 139.78313),
@@ -66,9 +65,9 @@ const offer = {
   checkout: getRandom(CheckOut),
   features: getRandom(Feat),
   description: "Большой дом",
-  photos: getRandom(PhoTos)
-}
+  photos: getRandom(PhoTos),
+};
 const location = {
   lat: random(35.65000,35.70000),
-  lng: random(139.70000, 139.80000)
-}
+  lng: random(139.70000, 139.80000),
+};
